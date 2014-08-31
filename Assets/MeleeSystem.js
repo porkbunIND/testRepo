1b291da72c38288ad8c755730aaa2309837d5c5a
Edmond Szeto <eszeto831@gmail.com>
2014-08-30 18:00:17 -0700

    initial commit

---
+++ Assets/MeleeSystem.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+var TheDamage : int = 50;
+var Distance : float;
+var MaxDistance : float = 1.5;
+var TheMace : Transform;
+
+
+function Update () 
+{
+	if (Input.GetButtonDown("Fire1")) 
+	{
+	
+		TheMace.animation.Play("Attack");
+	 	var hit : RaycastHit;
+	 	if (Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), hit))
+	 	{ 
+	 		Distance = hit.distance;
+	 		if (Distance < MaxDistance)
+	 		{
+	 			hit.transform.SendMessage ("ApplyDamage", TheDamage, SendMessageOptions.DontRequireReceiver);
+	 		}
+	 	}
+ 	}
+}
+
+
